a453d1ffe20dc0ec275819cd6f3fd883aa816974
Sergey Peshko <peshko.sergey@gmail.com>
2017-04-16 00:24:28 +0300

    bugfix

--- GPSSGenerator/StreamDemension/StreamModel.cs
+++ GPSSGenerator/StreamDemension/StreamModel.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Collections;
 using GPSSGenerator.Nodes;
+using GPSSGenerator.Nodes.Nodes;
+using GPSSGenerator.GlobalDimension;
 
 namespace GPSSGenerator.StreamDimension
 {
@@ -75,7 +77,7 @@ namespace GPSSGenerator.StreamDimension
 				graph.Add(links);
 			}
 
-			AnalisOfMatrx();
+			AnalisOfMatrix();
 		}
 		public void Show()
 		{
@@ -93,7 +95,7 @@ namespace GPSSGenerator.StreamDimension
 			}
 			Console.WriteLine();
 		}
-		private void AnalisOfMatrx()
+		private void AnalisOfMatrix()
 		{
 			for (int i = 0; i < graph.Count; i++)
 			{
@@ -102,9 +104,101 @@ namespace GPSSGenerator.StreamDimension
 				{
 					counter += graph[i][j];
 				}
-				if (counter > 2 && counter != 0)
+				if (counter != 1 && counter != 0)
 					throw new Exception("Matrix wrong");
 			}
+
+			int count = graph.Count;
+			for (int i = 0; i < count; i++)
+			{
+				for (int j = 0; j < count; j++)
+				{
+					if(graph[i][j] < 1 && graph[i][j] > 0)
+					{
+						InsertTransfers(i);
+						break;
+					}
+				}
+
+			}
+		}
+
+		private int InsertTransfers(int currNode)
+		{
+			List<KeyValuePair<int, double>> transitions = new List<KeyValuePair<int, double>>();
+			for (int i = 0; i < graph[currNode].Count; i++)
+			{
+				if (graph[currNode][i] > 0 && graph[currNode][i] < 1)
+				{
+					transitions.Add(new KeyValuePair<int, double>(i, graph[currNode][i]));
+					nodes[i].IsNeedLabel = true;
+				}
+			}
+			double remainingPersents = 1;
+			Transfer[] transfers = new Transfer[transitions.Count - 1];
+			for (int i = 0; i < transfers.Length; i++)
+			{
+				transfers[i] = new Transfer(string.Format("t{0}", GlobalVariables.counterOfTransfers++));
+				if (i > 0)
+					transfers[i].IsNeedLabel = true;
+			}
+			List<List<double>> insertlinks = new List<List<double>>();
+			for (int i = 0; i < transitions.Count - 1; i++)
+			{
+				double percents = (double)(transitions[i].Value * 1) / remainingPersents;
+				remainingPersents -= transitions[i].Value;
+				/*
+				transfers[i].SetParam(percents,
+					String.Format("Stream_{0}", index) + nodes[transitions[i].Key].Label,
+					i == transitions.Count - 2 ?
+					String.Format("Stream_{0}", index) + nodes[transitions[transitions.Count - 1].Key].Label :
+					String.Format("Stream_{0}", index) + transfers[i + 1].Label
+					);
+				*/
+				transfers[i].Probability1 = percents;
+				transfers[i].Node1 = nodes[transitions[i].Key];
+				if(i == transitions.Count - 2)
+				{
+					transfers[i].Node2 = nodes[transitions[transitions.Count - 1].Key];
+				}
+				else
+				{
+					transfers[i].Node2 = transfers[i + 1];
+				}
+				//nodes[transitions[i].Key].IsNeedLabel = true;
+
+				List<double> tmp = new List<double>();
+
+				for (int ii = 0; ii < (graph.Count + transitions.Count - 1); ii++)
+				{
+					tmp.Add(0);
+				}
+				tmp[transitions[i].Key] = percents;
+				tmp[i == transitions.Count - 2 ?
+					transitions[transitions.Count - 1].Key :
+					transitions[i].Key + 1] = 1 - percents;
+
+
+				insertlinks.Add(tmp);
+
+			}
+
+			for (int i = 0; i < graph.Count; i++)
+			{
+				graph[i].InsertRange(graph.Count, new double[transitions.Count - 1]);
+			}
+
+			for (int i = 0; i < graph[currNode].Count; i++)
+			{
+				graph[currNode][i] = 0;
+			}
+
+			graph[currNode][graph.Count] = 1;
+
+			nodes.InsertRange(nodes.Count, transfers);
+			graph.InsertRange(graph.Count, insertlinks);
+
+			return transfers.Length;
 		}
 	}
 }
